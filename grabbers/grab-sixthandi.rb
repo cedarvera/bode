29a6b807da4773e98550730f95a333774d428c77
Jon Yu <cedarvera@gmail.com>
2015-12-22 19:02:26 -0500

    Add a scraper for the sixth & i synagogue's website

---
+++ grabbers/grab-sixthandi.rb
@@ -0,0 +1,37 @@
+require_relative 'grabber'
+
+class GrabSixthAndI < Grabber
+  # get the array of urls to grab from
+  def grab_urls
+    # The url that list all the shows
+    [
+      "https://www.sixthandi.org/events/category/arts-entertainment/music/list/"
+    ]
+  end
+  # Go through each url to get the shows
+  def shows
+    self.grab_pages(self.grab_urls).map do |page|
+      # Looks like it is consistent in the classes it uses
+      # so grab what we need
+      page.search(".event-list-item").map do |elem|
+        # It is split into two blocks
+        # artists are in the class 'event-content'
+        headlinerNode = elem.at(".event-content > .event-content-wrap > h2 > a")
+        supportNode   = elem.at(".event-content > .event-content-wrap > h3")
+        # date is in the class 'event-list-details'
+        dateNode      = elem.at(".event-list-details > .event-details-wrap dd")
+        # convert the date so we can see the year
+        date = convert_date(dateNode.text)
+        # create the show object
+        {
+          :venue     => "Sixth & I Synagogue",
+          :date      => date,
+          :headliner => headlinerNode == nil ? "" : headlinerNode.text,
+          :support   => supportNode == nil ?   [] : supportNode.text.split(",")
+        }
+      end
+    end
+  end
+end
+
+p(GrabSixthAndI.new.shows)
