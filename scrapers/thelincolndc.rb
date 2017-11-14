aabbb2a73742b67924f419e814a5b648a306f55b
Jon Yu <cedarvera@gmail.com>
2017-11-14 00:53:27 -0500

    Fix the link to the 930's shows.  Split the Lincoln Theatre's shows to its own scraper as it no longer is part of the 930's site.

--- scrapers/thelincolndc.rb
+++ scrapers/thelincolndc.rb
@@ -3,7 +3,7 @@ require_relative "lib/scraper_base"
 class Scraper < ScraperBase
   # get the pages and return the resulting html
   def grab_pages
-    visit("http://www.930.com/")
+    visit("https://www.thelincolndc.com/")
     yield(page.html)
   end
   # Go through the page and find the the shows
@@ -12,7 +12,7 @@ class Scraper < ScraperBase
     # so grab what we need
     page.search(".vevent").map do |elem|
       {
-        venue:     "930 Club",
+        venue:     "The Lincoln Theatre",
         date:      grab_text(elem, ".dates", Date.today),
         headliner: grab_text(elem, ".headliners a", ""),
         support:   grab_text(elem, ".supports a", [])
