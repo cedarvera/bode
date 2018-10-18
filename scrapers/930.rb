6b44bfd6842df868bc09f2a24e25a22ba7c679c3
Jon Yu <cedarvera@gmail.com>
2018-10-18 11:56:55 -0400

    Fix days with multiple shows for 930 club

--- scrapers/930.rb
+++ scrapers/930.rb
@@ -10,13 +10,16 @@ class Scraper < ScraperBase
   def find_shows(page)
     # Looks like it is consistent in the classes it uses
     # so grab what we need
-    page.search(".upcoming-view.grid .vevent").map do |elem|
-      {
-        venue:     "930 Club",
-        date:      grab_text(elem, ".date-ymd", Date.today),
-        headliner: grab_text(elem, ".headliners a", ""),
-        support:   grab_texts(elem, ".supports a", "")
-      }
+    page.search(".upcoming-view.grid .vevent").map do |day|
+      date = grab_text(day, ".date-ymd", Date.today)
+      day.search(".one-event").map do |elem|
+        {
+          venue:     "930 Club",
+          date:      date,
+          headliner: grab_text(elem, ".headliners a", ""),
+          support:   grab_texts(elem, ".supports a", "")
+        }
+      end
     end
   end
 end
