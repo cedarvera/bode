14be1983fecef8f1de2ad30bd0af243882a2fcb0
Jon Yu <cedarvera@gmail.com>
2018-10-18 16:20:46 -0400

    Add more venues that use ticketfly's template
    - Milkboy Arthouse
    - The Anthem
    - The Miracle Theatre
    - Unionstage

---
+++ scrapers/themiracletheatre.rb
@@ -0,0 +1,22 @@
+require_relative "lib/scraper_base"
+
+class Scraper < ScraperBase
+  # get the pages and return the resulting html
+  def grab_pages
+    visit("http://themiracletheatre.ticketfly.com/")
+    yield(page.html)
+  end
+  # Go through the page and find the the shows
+  def find_shows(page)
+    # Looks like it is consistent in the classes it uses
+    # so grab what we need
+    page.search(".list-view .vevent").map do |elem|
+      {
+        venue:     "The Miracle Theatre",
+        date:      grab_text(elem, ".dates", Date.today),
+        headliner: grab_text(elem, ".headliners a", ""),
+        support:   grab_text(elem, ".supports a", [])
+      }
+    end
+  end
+end
---
+++ scrapers/unionstage.rb
@@ -0,0 +1,22 @@
+require_relative "lib/scraper_base"
+
+class Scraper < ScraperBase
+  # get the pages and return the resulting html
+  def grab_pages
+    visit("http://www.unionstage.com/listing")
+    yield(page.html)
+  end
+  # Go through the page and find the the shows
+  def find_shows(page)
+    # Looks like it is consistent in the classes it uses
+    # so grab what we need
+    page.search(".list-view .vevent").map do |elem|
+      {
+        venue:     "Union Stage",
+        date:      grab_text(elem, ".dates", Date.today),
+        headliner: grab_text(elem, ".headliners a", ""),
+        support:   grab_text(elem, ".supports a", [])
+      }
+    end
+  end
+end
